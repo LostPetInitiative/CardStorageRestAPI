3dae392872e13059597c82c983252ee4b18e2295
Dmitry A. Grechka <dmitry@grechka.family>
2022-09-02 20:47:18 +0300

    Create codeql-analysis.yml (#4)
    * Create codeql-analysis.yml
    
    * Adding inputs validation
    
    * permitting - and _ in the identifiers

--- Controllers/PetCardsController.cs
+++ Controllers/PetCardsController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using PatCardStorageAPI.Storage;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using CardStorageRestAPI;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,118 +24,164 @@ namespace PatCardStorageAPI.Controllers
 
         // GET <PetCardsController>/pet911ru/rf123
         [EnableCors]
-        [HttpGet("{ns}/{localID}")]
-        public async Task<ActionResult<PetCard>> Get(string ns, string localID, [FromQuery] bool includeSensitiveData = false)
+        [HttpGet("{nsStr}/{localIDstr}")]
+        public async Task<ActionResult<PetCard>> Get(string nsStr, string localIDstr, [FromQuery] bool includeSensitiveData = false)
         {
             try
             {
-                Trace.TraceInformation($"Getting card for {ns}/{localID}");
-                var result = await this.storage.GetPetCardAsync(ns, localID);
-                if (result == null)
+                // verifying inputs
+                AsciiIdentifier ns = new(nsStr);
+                AsciiIdentifier localID = new(localIDstr);
+
+                try
                 {
-                    Trace.TraceInformation($"card for {ns}/{localID} not found");
-                    return NotFound();
-                }
-                else
-                {
-                    Trace.TraceInformation($"Successfully retrieved card for {ns}/{localID}");
-                    if (!includeSensitiveData)
+
+                    Trace.TraceInformation($"Getting card for {ns}/{localID}");
+                    var result = await this.storage.GetPetCardAsync(ns, localID);
+                    if (result == null)
                     {
-                        Trace.TraceInformation($"Wiping out sensitive data for {ns}/{localID}");
-                        var contacts = result.ContactInfo;
-                        if (contacts != null) {
-                            contacts.Email = new string[0];
-                            contacts.Name = "";
-                            contacts.Tel = new string[0];
-                            contacts.Website = new string[0];
-                        }
+                        Trace.TraceInformation($"card for {ns}/{localID} not found");
+                        return NotFound();
                     }
                     else
                     {
-                        Trace.TraceInformation($"Returning sensitive data to the client for {ns}/{localID}");
+                        Trace.TraceInformation($"Successfully retrieved card for {ns}/{localID}");
+                        if (!includeSensitiveData)
+                        {
+                            Trace.TraceInformation($"Wiping out sensitive data for {ns}/{localID}");
+                            var contacts = result.ContactInfo;
+                            if (contacts != null)
+                            {
+                                contacts.Email = new string[0];
+                                contacts.Name = "";
+                                contacts.Tel = new string[0];
+                                contacts.Website = new string[0];
+                            }
+                        }
+                        else
+                        {
+                            Trace.TraceInformation($"Returning sensitive data to the client for {ns}/{localID}");
+                        }
+                        return new ActionResult<PetCard>(result);
                     }
-                    return new ActionResult<PetCard>(result);
+                }
+                catch (Exception err)
+                {
+                    Trace.TraceError($"Except card for {ns}/{localID}: {err}");
+                    return StatusCode(500, err.ToString());
                 }
             }
-            catch (Exception err)
-            {
-                Trace.TraceError($"Except card for {ns}/{localID}: {err}");
-                return StatusCode(500, err.ToString());
+            catch (ArgumentException ae) {
+                Trace.TraceError(ae.ToString());
+                return BadRequest(ae);
             }
         }
 
         
-        [HttpPut("{ns}/{localID}/features/{featuresIdent}")]
-        public async Task<IActionResult> PutFeatures(string ns, string localID, string featuresIdent, [FromBody] JsonPoco.FeaturesPOCO features)
+        [HttpPut("{nsStr}/{localIDstr}/features/{featuresIdentStr}")]
+        public async Task<IActionResult> PutFeatures(string nsStr, string localIDstr, string featuresIdentStr, [FromBody] JsonPoco.FeaturesPOCO features)
         {
             try
             {
-                Trace.TraceInformation($"Setting features {featuresIdent} for {ns}/{localID}");
-                await this.storage.SetCardFeatureVectorAsync(ns, localID, featuresIdent, features.Features);
-                Trace.TraceInformation($"Successfully set features {featuresIdent} for {ns}/{localID}");
-                return Ok();
+                // verifying inputs
+                AsciiIdentifier ns = new(nsStr);
+                AsciiIdentifier localID = new(localIDstr);
+                AsciiIdentifier featuresIdent = new(featuresIdentStr);
+                try
+                {
+                    Trace.TraceInformation($"Setting features {featuresIdent} for {ns}/{localID}");
+                    await this.storage.SetCardFeatureVectorAsync(ns, localID, featuresIdent, features.Features);
+                    Trace.TraceInformation($"Successfully set features {featuresIdent} for {ns}/{localID}");
+                    return Ok();
+                }
+                catch (Exception err)
+                {
+                    Trace.TraceError($"Except card for {ns}/{localID}: {err}");
+                    return StatusCode(500, err.ToString());
+                }
             }
-            catch (Exception err)
-            {
-                Trace.TraceError($"Except card for {ns}/{localID}: {err}");
-                return StatusCode(500, err.ToString());
+            catch (ArgumentException ae) {
+                Trace.TraceError(ae.ToString());
+                return BadRequest(ae);
             }
         }
-        
 
         // PUT <PetCardsController>
-        [HttpPut("{ns}/{localID}")]
+        [HttpPut("{nsStr}/{localIDstr}")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult> Put(string ns, string localID, [FromBody] PetCard value)
+        public async Task<ActionResult> Put(string nsStr, string localIDstr, [FromBody] PetCard value)
         {
             try
             {
-                Trace.TraceInformation($"Storing {ns}/{localID} into storage");
-                var res = await this.storage.SetPetCardAsync(ns, localID, value);
-                if (res)
-                {
-                    Trace.TraceInformation($"Stored {ns}/{localID}");
-                    return CreatedAtAction(nameof(Get),new { ns= ns, localID = localID },null);
-                }
-                else
-                {
-                    Trace.TraceWarning($"Card {ns}/{localID} already exists");
-                    return Conflict();
-                }
-            }
-            catch (Exception err)
-            {
-                Trace.TraceError($"Exception while storing {ns}/{localID}: {err}");
-                return StatusCode(500, err.ToString());
-            }
+                // verifying inputs
+                AsciiIdentifier ns = new(nsStr);
+                AsciiIdentifier localID = new(localIDstr);
 
+                try
+                {
+                    Trace.TraceInformation($"Storing {ns}/{localID} into storage");
+                    var res = await this.storage.SetPetCardAsync(ns, localID, value);
+                    if (res)
+                    {
+                        Trace.TraceInformation($"Stored {ns}/{localID}");
+                        return CreatedAtAction(nameof(Get),new { ns= ns, localID = localID },null);
+                    }
+                    else
+                    {
+                        Trace.TraceWarning($"Card {ns}/{localID} already exists");
+                        return Conflict();
+                    }
+                }
+                catch (Exception err)
+                {
+                    Trace.TraceError($"Exception while storing {ns}/{localID}: {err}");
+                    return StatusCode(500, err.ToString());
+                }
+            }
+            catch (ArgumentException ae)
+            {
+                Trace.TraceError(ae.ToString());
+                return BadRequest(ae);
+            }
         }
 
         // DELETE <PetCardsController>/pet911ru/rf123
-        [HttpDelete("{ns}/{localID}")]
-        public async Task<ActionResult> Delete(string ns, string localID)
+        [HttpDelete("{nsStr}/{localIDstr}")]
+        public async Task<ActionResult> Delete(string nsStr, string localIDstr)
         {
             try
             {
-                Trace.TraceInformation($"Received delete request for {ns}/{localID}");
-                var res = await this.storage.DeletePetCardAsync(ns, localID);
-                if (res)
+                // verifying inputs
+                AsciiIdentifier ns = new(nsStr);
+                AsciiIdentifier localID = new(localIDstr);
+
+                try
                 {
-                    Trace.TraceInformation($"Successfully deleted {ns}/{localID} from storage");
-                    return Ok();
+                    Trace.TraceInformation($"Received delete request for {ns}/{localID}");
+                    var res = await this.storage.DeletePetCardAsync(ns, localID);
+                    if (res)
+                    {
+                        Trace.TraceInformation($"Successfully deleted {ns}/{localID} from storage");
+                        return Ok();
+                    }
+                    else
+                    {
+                        Trace.TraceError($"Failed to delete {ns}/{localID}");
+                        return StatusCode(500);
+                    }
                 }
-                else
+                catch (Exception err)
                 {
-                    Trace.TraceError($"Failed to delete {ns}/{localID}");
-                    return StatusCode(500);
+                    Trace.TraceError($"Exception while deleting {ns}/{localID}: {err}");
+                    return StatusCode(500, err.ToString());
                 }
             }
-            catch (Exception err)
+            catch (ArgumentException ae)
             {
-                Trace.TraceError($"Exception while deleting {ns}/{localID}: {err}");
-                return StatusCode(500, err.ToString());
+                Trace.TraceError(ae.ToString());
+                return BadRequest(ae);
             }
         }
     }
---
+++ Security.cs
@@ -0,0 +1,31 @@
+﻿namespace CardStorageRestAPI
+{
+    public class AsciiIdentifier
+    {
+        private readonly string verifiedString;
+        public AsciiIdentifier(string str) {
+            // checking the input for permitted values
+
+            var trimmed = str.Trim();
+            // only A-Za-z0-9 and '_' and '-' are permitted
+            foreach (char c in trimmed) {
+                if (
+                    (c >= '0' && c <= '9') ||
+                    (c >= 'A' && c <= 'Z') ||
+                    (c >= 'a' && c <= 'z') ||
+                    (c == '-') || (c == '_')
+                    )
+                    continue;
+                throw new ArgumentException($"identifier contains characters that are not permitted. Only A-Za-z0-9 and '_' and '-' are permitted");
+            }
+            verifiedString = trimmed;
+        }
+
+        public override string ToString()
+        {
+            return this.verifiedString;
+        }
+
+
+    }
+}
