72773b437b576240ca270ffc52fc4d28b822aa33
Dmitry Grechka <dmitry@grechka.family>
2022-09-02 20:43:15 +0300

    permitting - and _ in the identifiers

--- Security.cs
+++ Security.cs
@@ -9,9 +9,14 @@
             var trimmed = str.Trim();
             // only A-Za-z0-9 and '_' and '-' are permitted
             foreach (char c in trimmed) {
-                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+                if (
+                    (c >= '0' && c <= '9') ||
+                    (c >= 'A' && c <= 'Z') ||
+                    (c >= 'a' && c <= 'z') ||
+                    (c == '-') || (c == '_')
+                    )
                     continue;
-                throw new ArgumentException($"identifier contains charaters that are not permitted. Only A-Za-z0-9 and '_' and '-' are permitted");
+                throw new ArgumentException($"identifier contains characters that are not permitted. Only A-Za-z0-9 and '_' and '-' are permitted");
             }
             verifiedString = trimmed;
         }
